1d3a4cabaa210b9e7352b3a62fe4805bf0b7e82b
VieiraGuilherme-dev <gv524003@gmail.com>
2025-08-26 23:36:50 -0300

    Pawn updated

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -13,9 +14,60 @@ public class Pawn extends ChessPiece {
     public String toString() {
         return "P";
     }
+
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        if (getColor() == Color.WHITE) {
+            // Movimento de 1 casa para frente
+            p.setValues(position.getRow() - 1, position.getColumn());
+            if (getBoard().positionExist(p) && !getBoard().thereIsAPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            // Movimento inicial de 2 casas para frente
+            p.setValues(position.getRow() - 2, position.getColumn());
+            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            if (getBoard().positionExist(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExist(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            // Captura na diagonal
+            p.setValues(position.getRow() - 1, position.getColumn() - 1);
+            if (getBoard().positionExist(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            p.setValues(position.getRow() - 1, position.getColumn() + 1);
+            if (getBoard().positionExist(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        } else { // Peça é preta
+            // Movimento de 1 casa para frente
+            p.setValues(position.getRow() + 1, position.getColumn());
+            if (getBoard().positionExist(p) && !getBoard().thereIsAPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            // Movimento inicial de 2 casas para frente
+            p.setValues(position.getRow() + 2, position.getColumn());
+            Position p2 = new Position(position.getRow() + 1, position.getColumn());
+            if (getBoard().positionExist(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExist(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            // Captura na diagonal
+            p.setValues(position.getRow() + 1, position.getColumn() - 1);
+            if (getBoard().positionExist(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            p.setValues(position.getRow() + 1, position.getColumn() + 1);
+            if (getBoard().positionExist(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
         return mat;
     }
 }
