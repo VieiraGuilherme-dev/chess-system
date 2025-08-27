0ccb2a7e48d80acb0a66f89ec6ac12d2c2aea2da
VieiraGuilherme-dev <gv524003@gmail.com>
2025-08-26 23:29:52 -0300

    Queen updated (and Pawn bugfix)

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -13,9 +14,98 @@ public class Queen extends ChessPiece {
     public String toString() {
         return "Q";
     }
+
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        // Movimento acima
+        p.setValues(position.getRow() - 1, position.getColumn());
+        while (getBoard().positionExist(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().thereIsAPiece(p) && isThereOpponentPiece(p)) {
+                break;
+            }
+            p.setValues(p.getRow() - 1, p.getColumn());
+        }
+
+        // Movimento abaixo
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while (getBoard().positionExist(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().thereIsAPiece(p) && isThereOpponentPiece(p)) {
+                break;
+            }
+            p.setValues(p.getRow() + 1, p.getColumn());
+        }
+
+        // Movimento à esquerda
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while (getBoard().positionExist(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().thereIsAPiece(p) && isThereOpponentPiece(p)) {
+                break;
+            }
+            p.setValues(p.getRow(), p.getColumn() - 1);
+        }
+
+        // Movimento à direita
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while (getBoard().positionExist(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().thereIsAPiece(p) && isThereOpponentPiece(p)) {
+                break;
+            }
+            p.setValues(p.getRow(), p.getColumn() + 1);
+        }
+
+        // Movimento Noroeste (NW)
+        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        while (getBoard().positionExist(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().thereIsAPiece(p) && isThereOpponentPiece(p)) {
+                break;
+            }
+            p.setValues(p.getRow() - 1, p.getColumn() - 1);
+        }
+
+        // Movimento Nordeste (NE)
+        p.setValues(position.getRow() - 1, position.getColumn() + 1);
+        while (getBoard().positionExist(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().thereIsAPiece(p) && isThereOpponentPiece(p)) {
+                break;
+            }
+            p.setValues(p.getRow() - 1, p.getColumn() + 1);
+        }
+
+        // Movimento Sudoeste (SW)
+        p.setValues(position.getRow() + 1, position.getColumn() - 1);
+        while (getBoard().positionExist(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().thereIsAPiece(p) && isThereOpponentPiece(p)) {
+                break;
+            }
+            p.setValues(p.getRow() + 1, p.getColumn() - 1);
+        }
+
+        // Movimento Sudeste (SE)
+        p.setValues(position.getRow() + 1, position.getColumn() + 1);
+        while (getBoard().positionExist(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().thereIsAPiece(p) && isThereOpponentPiece(p)) {
+                break;
+            }
+            p.setValues(p.getRow() + 1, p.getColumn() + 1);
+        }
+
         return mat;
     }
 }
